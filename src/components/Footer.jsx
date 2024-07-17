420a0a54cb07a34909c875e6dba088592bf61c87
Pragya Saxena <pragyasaxena8279@gmail.com>
2024-07-17 11:03:42 +0530

    created footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ import styles from './css/footer.module.css';
 export default function Footer() {
   return (
     <div id={styles.footer}>
+      <div id={styles.icons}>
         
+      </div>
+      <p id={styles.text} style={{marginBottom:'10px'}}>PRIVACY POLICY | TERMS OF USE</p>
+      <p id={styles.text}>@2007-2024 flipkart.com | ALL RIGHT RESERVED</p>
     </div>
   )
 }
